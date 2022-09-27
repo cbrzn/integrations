c1bda9db1c65dbb0781843f259479721b59a878f
cbrzn <cesarbrazon10@gmail.com>
2022-09-27 13:00:17 +0200

    chore: try to use reqwest in wrapper

--- system/http/rs/src/lib.rs
+++ system/http/rs/src/lib.rs
@@ -1,37 +1,20 @@
 pub mod wrap;
 
 use std::pin::Pin;
-use futures::{FutureExt, TryFuture, Future};
+use futures::executor;
 pub use wrap::*;
 
 /// Http Wrapper
 ///
 /// Polywrap wrapper to execute Http requests.
 
-type WrapperResponse = Pin<Box<dyn Future<Output=String>>>;
-
 async fn execute_request(url: String) -> String {
     let res = reqwest::get(url).await.unwrap();
     res.text().await.unwrap()
 }
 
-fn execute_request_wrapper<T>(url: String) -> Option<String> {
-    execute_request(url).now_or_never()
-}
-
-pub fn get(args: ArgsGet) -> Response {
-    match execute_request_wrapper::<String>(args.url) {
-        Some(data) => {
-            Response {
-                status: 200,
-                data,
-            }
-        }
-        _ => {
-            Response {
-                status: 200,
-                data: String::new(),
-            }
-        }
-    }
+pub fn get(args: ArgsGet) -> String {
+    let result = executor::block_on(execute_request(args.url));
+    
+    String::from(result)
 }
